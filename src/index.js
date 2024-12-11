13d7af4ffffe1258be6ff430c992f65c6a1135ff
RomanFS76 <korvy85@gmail.com>
2024-12-11 21:44:27 +0200

    add middleware 404

--- src/index.js
+++ src/index.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
