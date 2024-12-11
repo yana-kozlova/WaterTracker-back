14e4c2450619c96e924a481cb6321e85fca2b27c
RomanFS76 <korvy85@gmail.com>
2024-12-11 21:56:57 +0200

    add server.js, update index.js

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+
+const PORT = 3000;
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world!',
+    });
+  });
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: `Route ${req.method} ${req.originalUrl} not found`,
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
