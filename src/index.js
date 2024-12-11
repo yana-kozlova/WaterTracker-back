10182aca203fca17734aae390f1c43eb43112739
RomanFS76 <korvy85@gmail.com>
2024-12-11 21:42:55 +0200

    add middleware express.json

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 
+const PORT = 3000;
 const app = express();
 
-const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
