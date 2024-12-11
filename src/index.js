5c50e167696be69c1d062614088c1a8fcdaa3fc0
RomanFS76 <korvy85@gmail.com>
2024-12-11 21:38:56 +0200

    install express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
