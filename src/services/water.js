b5fbacd41da4f1ddc2cb06246da27b3da9d59e6e
RomanFS76 <korvy85@gmail.com>
2024-12-22 13:46:56 +0200

    update sservice/water.js

--- src/services/water.js
+++ src/services/water.js
@@ -2,15 +2,15 @@
 import UserCollection from '../db/models/User.js';
 import WaterCollection from '../db/models/Water.js';
 
+const today = new Date();
+const currentDate = today.toISOString().split('T')[0];
+const dateRegexp = new RegExp(`^${currentDate}`);
+
+
 export const addWater = async (payload) => {
   const { userId, date } = await WaterCollection.create(payload);
-  const waterList = await WaterCollection.find({ userId, date });
+  const waterItems = await WaterCollection.find({ userId, date });
   const { daily_norma } = await UserCollection.findOne(userId);
-
-  const today = new Date();
-  const currentDate = today.toISOString().split('T')[0];
-
-  const dateRegexp = new RegExp(`^${currentDate}`);
 
   const getServingsCount = await WaterCollection.aggregate([
     {
@@ -28,23 +28,17 @@ export const addWater = async (payload) => {
     },
   ]);
 
-
-const curDate = new Date(date);
-const month = curDate.toLocaleString('US-us', { month: 'short' });
-const day = curDate.getDate();
-const formattedDate = `${day}, ${month}`
-
-
-
-
-
+  const curDate = new Date(date);
+  const month = curDate.toLocaleString('US-us', { month: 'short' });
+  const day = curDate.getDate();
+  const formattedDate = `${day}, ${month}`;
 
   const servings = getServingsCount[0]?.count || 0;
   const totalAmount = getServingsCount[0]?.totalAmount || 0;
   const progress = Number(((totalAmount * 100) / daily_norma).toFixed(2));
-  const stats = { formattedDate, servings, totalAmount, progress};
+  const stats = { formattedDate, servings, totalAmount, progress };
 
-  return { waterList, stats };
+  return { waterItems, stats };
 };
 
 export const deleteWater = async ({ _id, userId }) => {
@@ -89,7 +83,7 @@ export const updateWater = async ({ _id, userId, date, amount, options = {} }) =
   const stats = { date, servings, totalAmount, progress };
 
   return {
-    data: {item:rawResult.value,stats},
+    data: { item: rawResult.value, stats },
     isNew: Boolean(rawResult.lastErrorObject.upserted),
   };
 };
