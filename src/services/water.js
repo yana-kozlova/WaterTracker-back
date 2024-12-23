7122f8e2934d4bd967bfb0e1cf1618cb3d37b8df
RomanFS76 <korvy85@gmail.com>
2024-12-24 01:47:03 +0200

    change routes water / update service getWater

--- src/services/water.js
+++ src/services/water.js
@@ -8,7 +8,25 @@ import WaterCollection from '../db/models/Water.js';
 
 export const getWater = async ({ userId }) => {
   const water = await WaterCollection.find({ userId });
-  return water;
+
+  const curDayLocal = new Date().toLocaleDateString('en-CA').split(',')[0];
+  const dateRegexp = new RegExp(`^${curDayLocal}`);
+
+  const waterList = await WaterCollection.aggregate([
+    {
+      $match: {
+        userId: { $eq: userId },
+        date: { $regex: dateRegexp },
+      },
+    },
+    {
+      $sort: {
+        date: 1,
+      },
+    },
+  ]);
+
+  return waterList;
 };
 
 
