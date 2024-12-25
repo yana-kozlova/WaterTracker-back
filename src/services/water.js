0b302e84a0087ac1c777aad0b85d9c9fe0ec9c21
RomanFS76 <korvy85@gmail.com>
2024-12-26 01:54:29 +0200

    update service/Water

--- src/services/water.js
+++ src/services/water.js
@@ -3,11 +3,10 @@ import UserCollection from '../db/models/User.js';
 import WaterCollection from '../db/models/Water.js';
 
 export const getWater = async ({ userId }) => {
-  const water = await WaterCollection.find({ userId });
 
   const curDayLocal = new Date().toLocaleDateString('en-CA').split(',')[0];
   const dateRegexp = new RegExp(`^${curDayLocal}`);
-
+  
   const waterList = await WaterCollection.aggregate([
     {
       $match: {
@@ -49,43 +48,6 @@ export const addWater = async (payload) => {
 };
 
 export const deleteWater = async ({ _id, userId }) => {
-  const { daily_norma } = await UserCollection.findOne(userId);
-
-  const today = new Date();
-  const currentDate = today.toISOString().split('T')[0];
-  const dateRegexp = new RegExp(`^${currentDate}`);
-
-  console.log(dateRegexp);
-  console.log(_id, userId);
-
-  const getServingsCount = await WaterCollection.aggregate([
-    {
-      $match: {
-        userId: { $eq: userId },
-        date: { $regex: dateRegexp },
-      },
-    },
-    {
-      $group: {
-        _id: null,
-        count: { $sum: 1 },
-        totalAmount: { $sum: '$amount' },
-      },
-    },
-  ]);
-
-  const curDate = new Date();
-  const month = curDate.toLocaleString('US-us', { month: 'short' });
-  const day = curDate.getDate();
-  const formattedDate = `${day}, ${month}`;
-
-  const servings = getServingsCount[0]?.count || 0;
-  const totalAmount = getServingsCount[0]?.totalAmount || 0;
-  const progress = Number(((totalAmount * 100) / daily_norma).toFixed(2));
-  const dayStats = { formattedDate, daily_norma, progress, servings };
-
-  console.log(dayStats);
-
   const data = await WaterCollection.findOneAndDelete({ _id, userId });
   return data;
 };
